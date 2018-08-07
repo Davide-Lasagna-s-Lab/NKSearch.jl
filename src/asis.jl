0618af122e890f4b1eb6d6f60a4759a8b5252926
Davide Lasagna <lasagnadavide@gmail.com>
2018-08-07 14:33:53 +0100

    final implementation

--- src/asis.jl
+++ src/asis.jl
@@ -9,38 +9,37 @@ export GradientCache,
        tovector
 
 # ////// METHODS THAT CUSTOM TYPES NEED TO OVERLOAD //////
-tovector!(out::Vector, ∇ᵤJ::NTuple{N, X}, ∇ₜJ::Real, ∇ₛJ::Real) where {N, X}=
+tovector!(out, dJdU0_L, dJdU0_R, ∇TLJ, ∇TRJ, ∇sJ) =
     throw(ArgumentError("not implemented"))
 
-tovector(∇ᵤJ::NTuple{N, X}, ∇ₜJ::Real, ∇ₛJ::Real) where {N, X} =
+tovector(dJdU0_L, dJdU0_R, ∇TLJ, ∇TRJ, ∇sJ) =
     throw(ArgumentError("not implemented"))
 
-fromvector!(∇ᵤJ::NTuple{N, X}, out::Vector) where {N, X} = 
+fromvector!(dJdU0_L, dJdU0_R, out) = 
     throw(ArgumentError("not implemented"))
 
 # associated helper function
-_checksize(out::Vector, ∇ᵤJ::NTuple{N, X}) where {N, X} =
-    length(out) == sum(length.(∇ᵤJ)) + 2 || 
+_checksize(out, dJdU0_L, dJdU0_R) =
+    length(out) == length(dJdU0_L) + length(dJdU0_R) + 3 || 
             throw(ArgumentError("inconsistent input size"))
 
 
 # ////// ACTUAL TYPE FOR CALCULATIONS //////
-mutable struct GradientCache{N, P, F, D, R, X, CS} 
-      ϕ::P  # nonlinear propagator
-  dϕdt!::F  # nonlinear equation right hand side
-   ddx!::D  # x derivative
-     ψ⁺::R  # linearised adjoint propagator
- caches::CS # a vector of stage caches filled by the nonlinear operator
-    TMP::Tuple{NTuple{N, X}, NTuple{N, X}, NTuple{N, X}, X, X} # temporaries
-      K::Int
+struct GradientCache{P, F, D, R, X, C} 
+      ϕ::P              # nonlinear propagator
+  dϕdt!::F              # nonlinear equation right hand side
+   ddx!::D              # x derivative
+     ψ⁺::R              # linearised adjoint propagator
+ caches::Tuple{C, C}    # two stage caches filled by the nonlinear operator
+    U0_C::X             # right initial condition
+    UF_C::X             # left terminal condition
+     TMP::NTuple{10, X} # temporaries
 end
 
-# Constructor. Pass U as extra parameter for creating the temporaries
+# constructor
 function GradientCache(ϕ, dϕdt!, ddx!, ψ⁺, 
-                       U::NTuple{N, X}, 
-                       caches::NTuple{N, C}, 
-                       K::Int=N) where {N, X, C<:AbstractStageCache}
-
+                       U0_C::X, UF_C::X,
+                       caches::Tuple{C, C}) where {X, C<:AbstractStageCache}
 
     # construct callable object
     cache = GradientCache(ϕ, 
@@ -48,14 +47,12 @@ function GradientCache(ϕ, dϕdt!, ddx!, ψ⁺,
                           ddx!, 
                           ψ⁺, 
                           caches,
-                          (deepcopy.(U), 
-                           deepcopy.(U), 
-                           deepcopy.(U),
-                           deepcopy(U[1]), 
-                           deepcopy(U[1])), K)
+                          U0_C,
+                          UF_C,
+                          ntuple(i->deepcopy(U0_C), 10))
 
     # preallocate buffer, with space for cost function and gradient
-    buffer = (zeros(1), tovector(U, 0, 0))
+    buffer = (zeros(1), tovector(U0_C, UF_C, 0, 0, 0))
 
     # use Optim.jl trick for shared calculations
     last_x = similar(buffer[2]); last_x[1] = rand()
@@ -80,67 +77,60 @@ end
 _prev_k(k, N) = (k+N-2)%N + 1
 _next_k(k, N) = (k+N)  %N + 1
 
-function (agc::GradientCache{N})(x::AbstractVector, buffer) where {N}
+function (agc::GradientCache)(x::AbstractVector, buffer)
     # ~~~ UNPACK LOCAL VARIABLES AND OBTAIN ALIASES ~~~
-    U₀, T, s = fromvector!(agc.TMP[1], x)
-    χ, ∇ᵤJ, Uₜ, dUₜdt = agc.TMP[2], agc.TMP[3], agc.TMP[4], agc.TMP[5]
-    ϕ        = agc.ϕ
-    dϕdt!    = agc.dϕdt!
-    ddx!     = agc.ddx!
-    ψ⁺       = agc.ψ⁺
-    caches   = agc.caches
+    U0_L, U0_R, TL, TR, s = fromvector!(agc.TMP[1], agc.TMP[2], x)
+    χ_L, χ_R, χ_C, UF_L, UF_R, TMP, dJdU0_L, dJdU0_R = agc.TMP[3:end]
+    U0_C   = agc.U0_C
+    UF_C   = agc.UF_C
+    ϕ      = agc.ϕ
+    dϕdt!  = agc.dϕdt!
+    ddx!   = agc.ddx!
+    ψ⁺     = agc.ψ⁺
+    caches = agc.caches
 
-    # shifts for every segment
-    sₖ = ntuple(i->(i == N ? s : zero(s)), N)
+    # ~~~ FINAL POINTS AND ERRORS ~~~
+    # χ_L
+    UF_L .= U0_L
+    ϕ(UF_L, (0, TL), reset!(caches[2]))
+    χ_L .= UF_L .- U0_C
 
-    # init data 
-    buffer[1][1] = 0.0
-    ∇ₜJ          = 0.0
-    ∇ₛJ          = 0.0
+    # χ_R
+    UF_R .= U0_R
+    shift!(ϕ(UF_R, (0, TR), reset!(caches[1])), s)
+    χ_R .= UF_R .- U0_L
 
-    # set to zero gradients that will not be calculated
-    for el in ∇ᵤJ; el .= 0; end
-    for el in χ;   el .= 0; end
-    
-    # ~~~ GRADIENT WRT TO INITIAL CONDITION ~~~
+    # χ_C
+    χ_C .= UF_C .- U0_R
 
-    # this loop is not thread safe, because ϕ writes to the 
-    # stage cache for ψ⁺ to read when integrating backwards. 
-    # One should create copies of ϕ, making sure copies of 
-    # the stage cache are done too. 
-    ks = unique(collect(chain(1:agc.K, (N-agc.K+1):N)))
+    # ~~~ TOTAL COST ~~~
+    buffer[1][1] = (dot(χ_C, χ_C) + dot(χ_R, χ_R) + dot(χ_L, χ_L))/2
 
-    for k in ks
-        # obtain (optionally) shifted end point
-        Uₜ .= U₀[k]
-        shift!(ϕ(Uₜ, (0, T/N), reset!(caches[k])), sₖ[k])
 
-        # obtain end point acceleration
-        dϕdt!(T/N, Uₜ, dUₜdt)
+    # ~~~ GRADIENT WRT INITIAL CONDITIONS ~~~
+    # gradient wrt U0_L
+    TMP .= χ_L
+    dJdU0_L .= ψ⁺(TMP, caches[2]) .- χ_R
 
-        # calc error
-        χ[k] .= Uₜ .- U₀[_next_k(k, N)]
+    # and wrt U0_R
+    TMP .= χ_R
+    dJdU0_R .= ψ⁺(shift!(TMP, -s), caches[1]) .- χ_C
 
-        # now update gradient wrt to T
-        ∇ₜJ += dot(χ[k], dUₜdt)/N
 
-        # and update gradient WRT to shift
-        if k == N
-            ∇ₛJ += dot(χ[k], ddx!(Uₜ))
-        end
-    end
-       
-    for k in ks
-        # update cost
-        buffer[1][1] += 0.5*dot(χ[k], χ[k])
+    # ~~~ GRADIENT WRT TIME AND SHIFT ~~~
+    # TL
+    ∇TLJ = dot(χ_L, dϕdt!(0, UF_L, TMP))
 
-        # calculate gradient wrt to initial conditions, aliasing over Uₜ
-        Uₜ .= χ[k]
-        ∇ᵤJ[k] .= ψ⁺(shift!(Uₜ, -sₖ[k]), caches[k]) .- χ[_prev_k(k, N)]
-    end
+    # TR
+    ∇TRJ = dot(χ_R, dϕdt!(0, UF_R, TMP))
 
+    # s
+    ∇sJ = dot(χ_R, ddx!(UF_R))
+
+
+    # ~~~ PACK AND RETURN ~~~
     # now obtain the other two gradients
-    tovector!(buffer[2], ∇ᵤJ, ∇ₜJ, ∇ₛJ)
+    tovector!(buffer[2], dJdU0_L, dJdU0_R, ∇TLJ, ∇TRJ, ∇sJ)
 
     return nothing
 end
