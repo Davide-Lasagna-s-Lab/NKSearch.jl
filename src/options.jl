e712d13d7e4bf7f98d397403ecd1e6d0009bd4ab
Davide Lasagna <lasagnadavide@gmail.com>
2019-01-09 22:41:21 +0000

    improved options

--- src/options.jl
+++ src/options.jl
@@ -9,18 +9,28 @@ export Options
 # ~~~ SEARCH OPTIONS FOR NEWTON ITERATIONS ~~~
 
 @with_kw struct Options
-    maxiter::Int        = 10           # maximum newton iteration number
-    io                  = STDOUT       # where to print stuff
-    skipiter::Int       = 0            # skip iteration between displays
-    verbose::Bool       = true         # print iteration status
-    x_tol::Float64      = 1e-10        # tolerance on initial state correction
-    e_tol::Float64      = 1e-10        # tolerance on initial state correction
-    ls_maxiter::Int     = 10           # maximum number of line search iterations 
-    ls_rho::Float64     = 0.5          # line search step reduction factor
-    gmres_maxiter::Int  = 10           # maximum number of GMRES iterations
-    gmres_verbose::Bool = true         # print GMRES iteration status
-    gmres_rtol::Float64 = 1e-3         # GMRES relative stopping tolerance
-    ϵ::Float64          = 1e-6         # dt for finite difference approximation
+    # generic parameters
+    method::Symbol       = :linesearch  # search method 
+    maxiter::Int         = 10           # maximum newton iteration number
+    io                   = stdout       # where to print stuff
+    skipiter::Int        = 0            # skip iteration between displays
+    verbose::Bool        = true         # print iteration status
+    zx_norm_tol::Float64 = 1e-10        # tolerance on correction
+    e_norm_tol::Float64  = 1e-10        # tolerance on residual
+    ϵ::Float64           = 1e-6         # dt for finite difference approximation
                                        # of the derivative of the flow operator
-    method::Symbol      = :linesearch  # search method 
+    # line search parameters
+    ls_maxiter::Int      = 10           # maximum number of line search iterations 
+    ls_rho::Float64      = 0.5          # line search step reduction factor
+
+    # GMRES parameters
+    gmres_maxiter::Int   = 10           # maximum number of GMRES iterations
+    gmres_verbose::Bool  = true         # print GMRES iteration status
+    gmres_rtol::Float64  = 1e-3         # GMRES relative stopping tolerance
+
+    # trust_region algorithm parameters
+    tr_radius_init::Float64 = 1        # initial trust region radius
+    tr_radius_max::Float64 = 10^8      # maximum trust region radius
+    eta::Float64 = 0.25                # maximum trust region radius
+
 end
