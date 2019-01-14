958f21399ac632870dc820b587fc87ce061e5ca1
Davide Lasagna <lasagnadavide@gmail.com>
2019-01-14 21:49:58 +0000

    can now solve problem with symmetries

--- src/direct_sol_cache.jl
+++ src/direct_sol_cache.jl
@@ -18,7 +18,7 @@ function op_apply_eye!(out::Matrix{T},
         _u    .= u
         _v    .= 0
         _v[i]  = 1
-        out[:, i] .= op(couple(_u, _v), span)[2]
+        out[:, i] .= op(_u, _v, span)
     end
     return out
 end
@@ -53,37 +53,68 @@ function update!(dsm::DirectSolCache,
     n = length(b[1])
 
     # aliases
-     A = dsm.A
+     A = dsm.A; A.nzval .= 0
      Y = dsm.Y
     _u = dsm.tmp[1]
     _v = dsm.tmp[2]
+     S = dsm.S
      L = dsm.L
      D = dsm.D
      G = dsm.G
      T = z0.d[1]
+     s = NS == 2 ? z0.d[2] : 0.0
+
+    op1(x, y, span) = (L(couple(x, y), span); y)
+    op2(x, y, span) = (L(couple(x, y), span); S(y, s); y)
 
     # fill main block
     for i = 1:N
         rng = _blockrng(i, n)
-        A[rng, rng] .= op_apply_eye!(Y, L, z0[i], (0, T/N), _u, _v)
+        if NS == 2 && i == N
+            op_apply_eye!(Y, op2, z0[i], (0, T/N), _u, _v)
+        else
+            op_apply_eye!(Y, op1, z0[i], (0, T/N), _u, _v)
+        end
+        A[rng, rng] .= Y
     end
 
     # add identities on diagonals
-    A[diagind(A,        n)] .= -1
-    A[diagind(A, -(N-1)*n)] .= -1
+    if N == 1
+        A[diagind(A,        0)] .+= -1
+    else
+        A[diagind(A,        n)] .+= -1
+        A[diagind(A, -(N-1)*n)] .+= -1
+    end
 
     # reset borders
-    A[end, :] .= 0
-    A[:, end] .= 0
+    A[end - NS + 1:end, :] .= 0
+    A[:, end - NS + 1:end] .= 0
 
     # lower borders
-    A[end, _blockrng(1, n)] .= D[1](_u, z0[1])
+    A[end - NS + 1, _blockrng(1, n)] .= D[1](_u, z0[1])
+    if NS == 2
+        A[end, _blockrng(1, n)] .= D[2](_u, z0[1])
+    end
 
     # right borders and right hand side
     for i = 1:N
         _u .= z0[i]
         G(_u, (0, T/N))
-        A[_blockrng(i, n), end] .= D[1](_v, _u)./N
+
+        _v .= z0[i]
+        G(_v, (0, T/N + opts.ϵ))
+
+        _v .= (_v .- _u)./(N .* opts.ϵ)
+        NS == 2 && i == N && S(_v, s)
+
+        A[_blockrng(i, n), end - NS + 1] .= _v
+
+        if NS == 2 && i == N
+            S(_u, s)
+            D[2](_v, _u)
+            A[_blockrng(i, n), end] .= _v
+        end
+
         b[i] .= z0[i % N + 1] .- _u
     end
 
