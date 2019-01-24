19e0b6772ffd12ff4383cfd8a0a64b0c5e307f60
Davide Lasagna <lasagnadavide@gmail.com>
2019-01-24 21:05:17 +0000

    multi-threaded code for parallel matvecs

--- src/iter_sol_cache.jl
+++ src/iter_sol_cache.jl
@@ -4,26 +4,28 @@
 import LinearAlgebra: dot
 import GMRES: gmres!
 import Flows
+using Base.Threads
 
 # ~~~ Matrix Type ~~~
-mutable struct IterSolCache{X, N, NS, GType, LType, SType, DType}
-        G::GType                                      # flow operator with no shifts
-        L::LType                                      # linearised flow operator with no shifts
-        S::SType                                      # space shift operator (can be NoShift)
-        D::DType                                      # time (and space) derivative operator
-       xT::NTuple{N, X}                               # time shifted conditions
-    dxTdT::NTuple{N, X}                               # time derivative of flow operator
-       z0::MVector{X, N, NS}                          # current orbit
-      mon::Flows.StoreOneButLast{X, typeof(copy)} # monitor
-      tmp::NTuple{2, X}                               # temporary storage
-     opts::Options                                    #
+mutable struct IterSolCache{X, N, NS, M, GType, LType, SType, DType}
+       Gs::Vector{GType}                                  # flow operator with no shifts
+       Ls::Vector{LType}                                  # linearised flow operator with no shifts
+        S::SType                                          # space shift operator (can be NoShift)
+        D::DType                                          # time (and space) derivative operator
+       xT::NTuple{N, X}                                   # time shifted conditions
+    dxTdT::NTuple{N, X}                                   # time derivative of flow operator
+       z0::MVector{X, N, NS}                              # current orbit
+     mons::Vector{Flows.StoreOneButLast{X, typeof(copy)}} # monitor
+      tmp::NTuple{M, X}                                   # temporary storage
+     opts::Options                                        #
 end
 
 # Main outer constructor
 IterSolCache(G, L, S, D, z0::MVector{X, N, NS}, opts) where {X, N, NS} =
-    IterSolCache(G, L, S, D, similar.(z0.x), similar.(z0.x), 
-                 similar(z0), Flows.StoreOneButLast(z0[1]), 
-                 ntuple(i->similar(z0[1]), 2), opts)
+    IterSolCache([deepcopy(G) for i = 1:N], 
+                 [deepcopy(L) for i = 1:N], S, D, similar.(z0.x), similar.(z0.x), 
+                 similar(z0), [Flows.StoreOneButLast(z0[1]) for i = 1:N], 
+                 ntuple(i->similar(z0[1]), 2N + 2), opts)
 
 # Main interface is matrix-vector product exposed to the Krylov solver
 Base.:*(mm::IterSolCache{X}, δz::MVector{X}) where {X} = mul!(similar(δz), mm, δz)
@@ -34,8 +36,7 @@ function mul!(out::MVector{X, N, NS},
                δz::MVector{X, N, NS}) where {X, N, NS}
     # aliases
     xT    = mm.xT
-    G     = mm.G
-    L     = mm.L
+    Ls    = mm.Ls
     D     = mm.D
     S     = mm.S
     z0    = mm.z0
@@ -45,15 +46,15 @@ function mul!(out::MVector{X, N, NS},
     ϵ     = mm.opts.ϵ
 
     # compute L{x0[i]}⋅δz[i] - δz[i+1]
-    for i = 1:N
+    Threads.@threads for i = 1:N
         # set perturbation initial condition
         out[i] .= δz[i]
 
         # set nonlinear initial condition
-        tmp[1] .= z0[i]
+        tmp[i] .= z0[i]
 
         # propagate by T/N
-        L(Flows.couple(tmp[1], out[i]), (0, T/N))
+        Ls[i](Flows.couple(tmp[i], out[i]), (0, T/N))
 
         # apply shift on last segment (if we have one)
         NS == 2 && i == N && S(out[i], z0.d[2])
@@ -87,25 +88,25 @@ function update!(mm::IterSolCache{X, N, NS},
 
     # aliases
     xT    = mm.xT
-    G     = mm.G
+    Gs    = mm.Gs
     S     = mm.S
     tmp   = mm.tmp
     dxTdT = mm.dxTdT
     ϵ     = opts.ϵ
     T     = z0.d[1]
-    mon   = mm.mon
+    mons  = mm.mons
 
     # update initial and final states
-    for i = 1:N
+    Threads.@threads for i = 1:N
         # set and propagate
         xT[i] .= z0[i]
-        G(xT[i], (0, T/N), mon)
+        Gs[i](xT[i], (0, T/N), mons[i])
 
         # finite difference derivative of flow operator 
-        tmp[1] .= mon.x; tmp[2] .= mon.x;
-        G(tmp[1], (mon.t, T/N + ϵ))
-        G(tmp[2], (mon.t, T/N - ϵ))
-        dxTdT[i] .= 0.5.*(tmp[1] .- tmp[2])./ϵ
+        tmp[i] .= mons[i].x; tmp[2i] .= mons[i].x;
+        Gs[i](tmp[ i], (mons[i].t, T/N + ϵ))
+        Gs[i](tmp[2i], (mons[i].t, T/N - ϵ))
+        dxTdT[i] .= 0.5.*(tmp[i] .- tmp[2i])./ϵ
     end
 
     # last one (may) get shifted
