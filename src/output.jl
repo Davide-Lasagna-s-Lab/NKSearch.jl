cec3eb26bfcbb2771a2b66dcfcf89ff1027e44ec
Davide Lasagna <lasagnadavide@gmail.com>
2019-01-26 14:07:46 +0000

    Update output.jl

--- src/output.jl
+++ src/output.jl
@@ -20,9 +20,9 @@ display_header_ls(io::IO, ::MVector{X, N, NS}) where {X, N, NS} =
     (print(io, _headers_ls[NS]); flush(io))
 
 # trust region
-const _header_tr = "+------+--------+-----------+-----------+------------+-----------+-----------+\n"*
-                   "| iter | which  |   step    |   ||e||   |    rho     | tr_radius |  ||dz||   |\n"*
-                   "+------+--------+-----------+-----------+------------+-----------+-----------+\n"
+const _header_tr = "+------+--------+-----------+-----------+------------+-----------+\n"*
+                   "| iter | which  |  ||dz||   |   ||e||   |    rho     | tr_radius |\n"*
+                   "+------+--------+-----------+-----------+------------+-----------+\n"
 
 display_header_tr(io::IO, ::MVector{X, N, NS}) where {X, N, NS} = 
     (print(_header_tr); flush(io))
@@ -48,8 +48,8 @@ function display_status_ls(io::IO, iter, dz_norm, d::Tuple{Any}, e_norm, λ, res
 end
 
 # trust region
-function display_status_tr(io::IO, iter, which, step, e_norm, rho, tr_radius, dz_norm)
-    str = @sprintf "|%4d  | %s | %5.3e | %5.3e | %+5.3e | %5.3e | %5.3e |" iter lpad(which, 6) step e_norm rho tr_radius dz_norm
+function display_status_tr(io::IO, iter, which, dz_norm, e_norm, rho, tr_radius)
+    str = @sprintf "|%4d  | %s | %5.3e | %5.3e | %+5.3e | %5.3e |" iter lpad(which, 6) dz_norm e_norm rho tr_radius
     println(str)
     flush(stdout)
 end
